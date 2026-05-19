fc928016997f724152a154b8181e7c3718296cbd
vikingokvist <vikingokvist@gmail.com>
2026-05-19 20:59:25 +0200

    ....

--- include/malloc.h
+++ include/malloc.h
@@ -1,9 +1,9 @@
 #ifndef MALLOC_H
-# define MALLOC_H
+#   define MALLOC_H
 
-# define PUBLIC __attribute__((visibility("default")))
+#define PUBLIC __attribute__((visibility("default")))
 
-# include <stddef.h>
+#include <stddef.h>
 
 // Allocate SIZE bytes of memory.
 PUBLIC void     *malloc(size_t size);
@@ -18,31 +18,31 @@ PUBLIC void      show_alloc_mem(void);
 PUBLIC void      show_alloc_mem_ex(void);
 // General interface to tunable parameters.
 // 
-// MALLOC_CHECK: M_CHECK_SILENT | M_CHECK_PRINT | M_CHECK_ABORT (uint8_t)
-// MALLOC_PERTURB: M_PERTURB_NONE - UCHAR_MAX (uint8_t)
-// MALLOC_ARENA_MAX: M_ARENA_MAX_DEFAULT - UINT_MAX (uint32_t)
 // MALLOC_SHOW_INFO: M_SHOW_INUSE | M_SHOW_ALL
+// MALLOC_CHECK: M_CHECK_SILENT | M_CHECK_PRINT | M_CHECK_ABORT 
+// MALLOC_PERTURB: M_PERTURB_NONE - UCHAR_MAX 
+// MALLOC_ARENA_MAX: M_ARENA_MAX_DEFAULT - UINT_MAX (uint32_t)
 PUBLIC int       mallopt(int param, int value);
 
  // MALLOC_ZERO
 #define MALLOC_SHOW_INFO    0x0
-#define M_SHOW_INUSE        (uint8_t)0
-#define M_SHOW_ALL          (uint8_t)1
-#define M_SHOW_DEFAULT      (uint8_t)0
+#define M_SHOW_INUSE        0
+#define M_SHOW_ALL          1
+#define M_SHOW_DEFAULT      0
 
  // _MALLOC_CHECK_
 #define MALLOC_CHECK        0x1
-#define M_CHECK_SILENT     (uint8_t)0
-#define M_CHECK_PRINT      (uint8_t)1
-#define M_CHECK_ABORT      (uint8_t)2
-#define M_CHECK_DEFAULT    (uint8_t)3
+#define M_CHECK_SILENT      0
+#define M_CHECK_PRINT       1
+#define M_CHECK_ABORT       2
+#define M_CHECK_DEFAULT     3
 
  // _MALLOC_PERTURB_
 #define MALLOC_PERTURB      0x2
-#define M_PERTURB_NONE   (uint8_t)0
+#define M_PERTURB_NONE      0
 
  // _MALLOC_ARENA_MAX_
 #define MALLOC_ARENA_MAX     0x3
-#define M_ARENA_MAX_DEFAULT  (uint32_t)65530
+#define M_ARENA_MAX_DEFAULT  65530
 
 #endif
